f7b846db2c6eb2125ff5b77e2211202deffeee3b
tacilaiasmin7 <tacilaiasmin180@gmail.com>
2025-09-23 11:33:41 -0300

    Aula Barbara- app Tempo

---
+++ tempo/src/service.worker.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open("app-temposync-cache").then(cache => {
+            return cache.addAll([
+                "index.html",
+                "manifest.json",
+                "icone-192.png",
+                "icon-512.png",
+                "manha.jpg",
+                "tarde.jpg",
+                "noite.jpg",
+                "madrugada.jpg"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWidth(
+        caches.match(event.request). then (response => response || fetch(event.request))
+    );
+});
